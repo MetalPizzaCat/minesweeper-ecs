04bd0afb992986bbf0027b53410f624ca339a5d6
CreativeMetalCat <catguy228@gmail.com>
2022-06-16 22:37:23 +1000

    added flood reveal

--- src/main.rs
+++ src/main.rs
@@ -46,23 +46,36 @@ pub fn render_game(
 struct Field {
     value: i32,
     bomb: bool,
+    border: bool,
+    revealed: bool,
 }
 
 fn generate_grid(area_size: usize, bomb_count: u32) -> Vec<Vec<Field>> {
     let mut rng = rand::thread_rng();
     //generate default grid
     let mut grid: Vec<Vec<Field>> = vec![vec![Field::default(); area_size]; area_size];
+    let mut bombs: Vec<Vector2<usize>> = Vec::new();
 
     let mut bomb_count = bomb_count;
     let die = Uniform::from(0..area_size);
     //generate all bombs
+    //we simply pick a random position and then if the position is taken
+    // we move diagonally to the left bottom until we hit a good spot
+    //if we reach the corner we move the line down and start from almost top right corner doing the same thing
     while bomb_count > 0 {
         let mut point: Vector2<usize> = Vector2::new(die.sample(&mut rng), die.sample(&mut rng));
         while grid[point.x][point.y].bomb {
-            point.x += 1;
-            point.y += 1
+            //to prevent going over the border because line was randomly generated
+            if point.x > area_size || point.y > area_size {
+                point.x = 1;
+                point.y = 0;
+            } else {
+                point.x += 1;
+                point.y += 1
+            }
         }
         grid[point.x][point.y].bomb = true;
+        bombs.push(point);
         bomb_count -= 1;
     }
     //calculate values for bombs
@@ -81,13 +94,58 @@ fn generate_grid(area_size: usize, bomb_count: u32) -> Vec<Vec<Field>> {
                         if hor >= area_size as i32 || hor < 0 {
                             continue;
                         }
+                        //we ignore the fact that the tile is a bomb and still add value 
+                        //because bomb with value is still a bomb, duh
                         grid[vert as usize][hor as usize].value += 1;
+                        //mark this tile as border
+                        if !grid[vert as usize][hor as usize].bomb {
+                            grid[vert as usize][hor as usize].border = true;
+                        }
                     }
                 }
             }
         }
     }
     grid
+}
+
+///Reveal the tile and all neighboring 0 tiles using flood algorithm
+fn reveal_block(
+    point: Vector2<i32>,
+    grid: &mut Vec<Vec<Field>>,
+    world: &mut World,
+    buttons: &Vec<Vec<Entity>>,
+) {
+    //simple border check
+    if point.x < 0 || point.x >= grid.len() as i32 || point.y < 0 || point.y >= grid.len() as i32 {
+        return;
+    }
+    let x = point.x as usize;
+    let y = point.y as usize;
+    //don't reveal bombs
+    if grid[x][y].bomb || grid[x][y].revealed {
+        return;
+    }
+    //update tile state so it would be drawn
+    {
+        if let Some(tile) = world.write_component::<Tile>().get_mut(buttons[x][y]) {
+            tile.revealed = true;
+        }
+        if let Some(text) = world.write_component::<Text>().get_mut(buttons[x][y]) {
+            text.visible = true;
+        }
+        //mark tile as visited
+        grid[x][y].revealed = true;
+    }
+    //if this is a border we want to display the tile itself, but not go any further
+    if grid[x][y].border {
+        return;
+    }
+    //reveal neighbors
+    reveal_block(point + Vector2::new(0, -1), grid, world, buttons);
+    reveal_block(point + Vector2::new(-1, 0), grid, world, buttons);
+    reveal_block(point + Vector2::new(0, 1), grid, world, buttons);
+    reveal_block(point + Vector2::new(1, 0), grid, world, buttons);
 }
 
 fn main() -> Result<(), String> {
@@ -110,8 +168,9 @@ fn main() -> Result<(), String> {
     let font = ttf_context
         .load_font("./assets/fonts/Roboto-Medium.ttf", 22)
         .unwrap();
-    let grid = generate_grid(10, 10);
+    let mut grid = generate_grid(10, 10);
     let mut buttons: Vec<Vec<Entity>> = Vec::new();
+    //generate button entities
     for i in 0..area_size {
         buttons.push(Vec::new());
         for j in 0..area_size {
@@ -127,7 +186,12 @@ fn main() -> Result<(), String> {
                         hovered_over_color: Some(sdl2::pixels::Color::RGBA(255, 0, 255, 120)),
                         normal_texture_name: None,
                         normal_text: None,
-                        normal_color: Some(sdl2::pixels::Color::RGBA(255, 255, 255, 120)),
+                        normal_color: Some(sdl2::pixels::Color::RGBA(
+                            if grid[i][j].border { 0 } else { 255 },
+                            255,
+                            255,
+                            120,
+                        )),
                     }),
                     sdl2::pixels::Color::RGBA(255, 255, 255, 120),
                     layers::RenderLayers::Menu,
@@ -139,7 +203,7 @@ fn main() -> Result<(), String> {
                         grid[i][j].value.to_string()
                     },
                     color: sdl2::pixels::Color::BLACK,
-                    visible: false,
+                    visible: grid[i][j].bomb,
                     offset: Vector2::new(20, 15),
                 })
                 .with(Tile {
@@ -165,17 +229,13 @@ fn main() -> Result<(), String> {
                 Event::MouseButtonDown {
                     x, y, mouse_btn, ..
                 } => {
-                    if let Some(tile) = ui::get_overlapping_component_with_type::<Tile>(
-                        Vector2::new(x, y),
-                        world.system_data(),
-                    ) {
-                        if let Some(text) = world
-                            .write_component::<Text>()
-                            .get_mut(buttons[tile.position.x][tile.position.y])
-                        {
-                            text.visible = true;
-                        }
-                    }
+                    //x and y are swapped because i accidentally swapped them in memory
+                    reveal_block(
+                        Vector2::new(y / 50, x / 50),
+                        &mut grid,
+                        &mut world,
+                        &buttons,
+                    );
                 }
                 _ => {}
             }
